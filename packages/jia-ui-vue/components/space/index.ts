ae141c9ec9b13f21e6dd47e2e99f6bd1e7ef5afd
jiadaoka <22958910+jiadaoka@users.noreply.github.com>
2023-02-28 18:21:00 +0800

    feat(components): 编写space组件

---
+++ packages/jia-ui-vue/components/space/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from 'jia-utils'
+import space from './src/space'
+
+const jiaButton = withInstall(space)
+
+export default jiaButton
---
+++ packages/jia-ui-vue/components/space/src/space.tsx
@@ -0,0 +1,21 @@
+import { defineComponent, renderSlot } from 'vue'
+import globalConfig from '../../../common/index'
+const cssName = globalConfig.cssName!.createBlock('space')
+
+export default defineComponent({
+    name: 'JiaSpace',
+    setup(props, { slots }) {
+        return () => {
+            const children = renderSlot(slots, 'default')
+
+            if (!Array.isArray(children.children) || children.children.length === 0) return <div class={[cssName.base(), cssName.m('empty')]}></div>
+            const itemCss = cssName.plus('item')
+            const domList: JSX.Element[] = []
+            children.children.forEach((item) => {
+                domList.push(<div class={[itemCss.base()]}>{item}</div>)
+            })
+
+            return <div class={[cssName.base()]}>{domList}</div>
+        }
+    },
+})
